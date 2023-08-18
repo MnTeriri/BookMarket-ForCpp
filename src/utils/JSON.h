b23391507b9b2c77a0f13b3e18c33b34b3e62348
Teriri <lsy1511736399@outlook.com>
2023-08-18 23:15:20 +0800

    JSON工具类

--- src/utils/JSON.h
+++ src/utils/JSON.h
@@ -4,6 +4,7 @@
 #include <QJsonArray>
 #include <QJsonObject>
 #include <QMetaProperty>
+#include <QMetaTypeId>
 
 class JSON {
 public:
@@ -11,26 +12,64 @@ public:
     static QJsonArray toJSONArray(QVector<T *> list) {
         QJsonArray jsonArray;
         for (int i = 0; i < list.size(); i++) {
-            jsonArray.append(toJSONObject(list[i]));
+            jsonArray.append(toJSONObject((QObject*)list[i]));//调用的QJsonObject toJSONObject(QObject *object)
         }
         return jsonArray;
     }
 
-    template<class T>
-    static QJsonObject toJSONObject(T *object) {
+    static QJsonArray toJSONArray(QVariant list) {
+        QJsonArray jsonArray;
+        QList<QVariant> listData = list.toList();
+        for (int i = 0; i < listData.size(); i++) {
+            jsonArray.append(toJSONObject(listData[i]));//QJsonObject toJSONObject(QVariant object)
+        }
+        return jsonArray;
+    }
+
+    static QJsonObject toJSONObject(QVariant object) {
         QJsonObject qJsonObject;
-        QMetaObject metaObject = T::staticMetaObject;
+        QMetaType metaType = object.metaType();//获取QVariant存储数据的QMetaType
+        QMetaObject metaObject = *metaType.metaObject();//根据QMetaType对象就能获取自定义类的QMetaObject对象了
+        qDebug() << metaObject.className();
         for (int i = metaObject.propertyOffset(); i < metaObject.propertyCount(); i++) {
-            QMetaProperty propertyName = metaObject.property(i);//获取属性名称
-            QVariant propertyValue = object->property(propertyName.name());//获取属性值
-            if (propertyName.typeId() == QMetaType::Type::QDateTime) {
-                //QDateTime转换为QString
-                qJsonObject[propertyName.name()] = propertyValue.toString();
-            } else if (propertyName.typeId() == QMetaType::Type::QByteArray) {
-                //QByteArray进行Base64加密，转QString
-                qJsonObject[propertyName.name()] = QString::fromUtf8(propertyValue.toByteArray().toBase64());
+            QMetaProperty property = metaObject.property(i);//获取属性名称
+            QVariant propertyValue = property.read(object.value<QObject *>());//获取属性值
+            if (QString(property.typeName()).startsWith("QList")) {//如果是QList/QVector
+                //qDebug() << property.name() << "是" << property.typeName() << "是QList";
+                qJsonObject[property.name()] = toJSONArray(propertyValue);
+            } else if (QMetaType::typeFlags(property.typeId()) & QMetaType::TypeFlag::IsPointer) {//如果是指针类型
+                //qDebug() << property.name() << "是" << property.typeName() << "是指针";
+                qJsonObject[property.name()] = toJSONObject(propertyValue);
+            } else if (property.typeId() == QMetaType::Type::QDateTime) {//QDateTime转换为QString
+                qJsonObject[property.name()] = propertyValue.toString();
+            } else if (property.typeId() == QMetaType::Type::QByteArray) {//QByteArray进行Base64加密，转QString
+                qJsonObject[property.name()] = QString::fromUtf8(propertyValue.toByteArray().toBase64());
             } else {
-                qJsonObject[propertyName.name()] = propertyValue.toJsonValue();
+                qJsonObject[property.name()] = propertyValue.toJsonValue();
+            }
+        }
+        return qJsonObject;
+    }
+
+    static QJsonObject toJSONObject(QObject *object) {
+        QJsonObject qJsonObject;
+        QMetaObject metaObject = *object->metaObject();
+        qDebug() << metaObject.className();
+        for (int i = metaObject.propertyOffset(); i < metaObject.propertyCount(); i++) {
+            QMetaProperty property = metaObject.property(i);//获取属性名称
+            QVariant propertyValue = object->property(property.name());//获取属性
+            if (QString(property.typeName()).startsWith("QList")) {//如果是QList/QVector
+                //qDebug() << property.name() << "是" << property.typeName() << "是QList";
+                qJsonObject[property.name()] = toJSONArray(propertyValue);
+            } else if (QMetaType::typeFlags(property.typeId()) & QMetaType::TypeFlag::IsPointer) {//如果是指针类型
+                //qDebug() << property.name() << "是" << property.typeName() << "是指针";
+                qJsonObject[property.name()] = toJSONObject(propertyValue.value<QObject *>());
+            } else if (property.typeId() == QMetaType::Type::QDateTime) {//QDateTime转换为QString
+                qJsonObject[property.name()] = propertyValue.toString();
+            } else if (property.typeId() == QMetaType::Type::QByteArray) {//QByteArray进行Base64加密，转QString
+                qJsonObject[property.name()] = QString::fromUtf8(propertyValue.toByteArray().toBase64());
+            } else {
+                qJsonObject[property.name()] = propertyValue.toJsonValue();
             }
         }
         return qJsonObject;
