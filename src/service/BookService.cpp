8a3b19d0f02f3261556c064a044b4b1df0b9fb43
Teriri <lsy1511736399@outlook.com>
2023-08-18 23:15:34 +0800

    书籍服务层

--- src/service/BookService.cpp
+++ src/service/BookService.cpp
@@ -16,3 +16,15 @@ QJsonObject BookService::getBookList(QString bname, int page, int count) {
     qDeleteAll(list);
     return jsonObject;
 }
+
+QVariant BookService::getBookList(QJsonObject param) {
+    QJsonObject jsonObject;
+    QVector<Book *> list = bookDao.getBookList(param["bname"].toString(), param["page"].toInt(), param["count"].toInt());
+    int recordsTotal = bookDao.getRecordsTotal();
+    int recordsFiltered = bookDao.getRecordsFiltered(param["bname"].toString());
+    jsonObject["bookList"] = JSON::toJSONArray(list);
+    jsonObject["recordsTotal"] = recordsTotal;
+    jsonObject["recordsFiltered"] = recordsFiltered;
+    qDeleteAll(list);
+    return jsonObject;
+}
--- src/service/BookService.h
+++ src/service/BookService.h
@@ -3,18 +3,21 @@
 
 #include <QObject>
 #include <QJsonArray>
+#include <QMetaType>
 #include "../model/Book.h"
 #include "../dao/BookDao.h"
 
 class BookService: public QObject{
     Q_OBJECT
 public:
-    explicit BookService(QObject *parent = nullptr);
+    Q_INVOKABLE explicit BookService(QObject *parent = nullptr);
 private:
     BookDao bookDao;
-public slots:
-    QJsonObject getBookList(QString bname, int page, int count);
+public:
+    Q_INVOKABLE QJsonObject getBookList(QString bname, int page, int count);
+    Q_INVOKABLE QVariant getBookList(QJsonObject param);
 };
 
+Q_DECLARE_METATYPE(BookService)
 
 #endif //BOOKMARKET_BOOKSERVICE_H
